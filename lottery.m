f4906fe663faf7595607b683ba7904e253c07ad6
Jiaji "George" Hu <georgehujj@gmail.com>
2013-07-12 10:15:53 +0800

    created lottery app, participant data stored in list.mat

---
+++ lottery.m
@@ -0,0 +1,28 @@
+%%  DA 96 graduation ceremony lottery
+% Created 2013.07.12
+clear
+clc
+%% 
+load list.mat
+
+ticket_number=6;
+participant_number=size(stuNum,1);
+
+fid=fopen('lottery_results.txt','w+');
+fprintf(fid,'Participants:\r\n\r\n');
+for i=1:participant_number
+    fprintf(fid,'%d %s\r\n',stuNum(i),name{i});
+end
+
+order=randperm(participant_number);
+
+winner=zeros(ticket_number,1);
+
+fprintf(fid,'\r\nWinners:\r\n\r\n');
+disp(sprintf('Winners: \n'));
+for i=1:6;
+    fprintf(fid,'%d %s\r\n',stuNum(order(i)),name{order(i)});
+    disp(sprintf('%d %s',stuNum(order(i)),name{order(i)}));
+end
+
+fclose(fid);
